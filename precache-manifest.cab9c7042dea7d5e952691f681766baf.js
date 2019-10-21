492ee057208ff0925416ef218791c30c7eeb0dd8
surferzer <mafernandazr@gmail.com>
2019-10-21 17:30:24 -0500

    Updates

---
+++ precache-manifest.cab9c7042dea7d5e952691f681766baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd268f4d17212f3a744b564beca39b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dc46cfa5da783f535ed",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "205077743e1bc62837b9",
+    "url": "/static/js/2.c67c5a6f.chunk.js"
+  },
+  {
+    "revision": "7dc46cfa5da783f535ed",
+    "url": "/static/js/main.93d72db5.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
