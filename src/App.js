4e8b499992b43f538556ebccf787afc280b61d4f
surferzer <mafernandazr@gmail.com>
2019-10-21 17:55:37 -0500

    se agrega logo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor(props) {
+super(props)
+
+}
+
+
+render(){
+  return (
+    <div className="App">
+<div className="App-logo">
+<logo.svg />
+</div>
+
+
+    </div>
+  )}}
+
+export default App;
