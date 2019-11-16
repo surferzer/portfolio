831b53a04a0f8cb1fb56295d14568fba8d5882e8
surferzer <mafernandazr@gmail.com>
2019-11-15 18:42:05 -0600

    se agregan fotos de cada proyecto

--- src/proyects.js
+++ src/proyects.js
@@ -15,15 +15,31 @@ class Proyects extends React.Component {
                <img src="https://i.ibb.co/88t7p7t/data-Lovers-2.png" width="50%" alt="dataLovers"/> </a>
            </div>
 
+           <div className='socialNetwork'>
+               <a href="https://cstgarcia1204.githum.io/GDL003-social-network/src/" tittle="socialNetwork">
+               <img src="https://i.imgur.com/EHbKcaC.jpg" width="30%" alt="socialNetwork"/> </a>
+           </div>
+
            <div className='labNotes'>
-               <a href="" tittle="labNotes">
-               <img src="" alt="labNotes"/> </a>
+               <a href="https://surferzer.github.io/GDL003-lab-notes/" tittle="labNotes">
+               <img src="https://i.ibb.co/Jj1K1JX/labnotes-2.png" width="50%" alt="labNotes"/> </a>
            </div>
 
            <div className='ciboulette'>
                <a href="https://lauramayar.github.io/GDL003-Burger-Queen-Front-End/" tittle="ciboulette">
                <img src="https://i.ibb.co/G360XKF/ciboulette-2.png" width="50%" alt="ciboulette"/> </a>
            </div>
+
+           <div className='competitionSystem'>
+               <a href="https://surferzer.github.io/GDL003-personal-project/" tittle="competitionSystem">
+               <img src="https://i.ibb.co/0XThBbb/competition-Sistem-2.png" width="50%" alt="competitionSystem"/> </a>
+           </div>
+           
+
+
+
+
+
 
            
        </div>
