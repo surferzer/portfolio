f7e07ef3efe448eea2be97dd2707516f1f75f864
surferzer <mafernandazr@gmail.com>
2019-11-22 16:44:31 -0600

    se agregan proyectos ordenados

--- src/proyects.js
+++ src/proyects.js
@@ -3,7 +3,9 @@ import { CSSTransitionGroup } from 'react-transition-group';
 import './proyects.css'
 
 
+
 class Proyects extends React.Component {
+    
     render(){
         return(
 
@@ -21,36 +23,133 @@ class Proyects extends React.Component {
             </p>
         </CSSTransitionGroup>
             
+
+
+<div className="cardStyle">
+    
+
+
+    <div className='card text-center shadow'>
+      <div className='overflow'>
+        <a href="https://surferzer.github.io/GDL003-data-lovers/src/"><img src="https://i.ibb.co/88t7p7t/data-Lovers-2.png" width="60%" alt='DataLovers' className='card-img-top'/></a>
+      </div>
+      <div className='card-body text-dark'>
+        <h4 className='card-title'>DataLovers</h4>
+        <p className='card-text text-secondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          distinctio maxime, sapiente temporibus cum quisquam totam voluptatum
+          asperiores. Necessitatibus, repellendus!
+        </p>
+        <a href="https://surferzer.github.io/GDL003-data-lovers/src/" className='btn btn-outline-success'>
+          Go inside
+        </a>
+      </div>
+      </div>
+   
+
+      <div className='card text-center shadow'>
+      <div className='overflow'>
+     <a href="https://surferzer.github.io/GDL003-lab-notes/"><img src="https://i.ibb.co/Jj1K1JX/labnotes-2.png" width="60%" alt='DataLovers' className='card-img-top'/></a>
+      </div>
+      <div className='card-body text-dark'>
+        <h4 className='card-title'>Lab Notes</h4>
+        <p className='card-text text-secondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          distinctio maxime, sapiente temporibus cum quisquam totam voluptatum
+          asperiores. Necessitatibus, repellendus!
+        </p>
+        <a href="https://surferzer.github.io/GDL003-lab-notes/" className='btn btn-outline-success'>
+          Go inside
+        </a>
+      </div>
+    
+    </div>
+
+
+
+    <div className='card text-center shadow'>
+      <div className='overflow'>
+      
+        <a href="https://lauramayar.github.io/GDL003-Burger-Queen-Front-End/">
+            <img src="https://i.ibb.co/G360XKF/ciboulette-2.png" width="60%" alt='DataLovers' className='card-img-top'/></a>
+      </div>
+      <div className='card-body text-dark'>
+        <h4 className='card-title'>Ciboulette</h4>
+        <p className='card-text text-secondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          distinctio maxime, sapiente temporibus cum quisquam totam voluptatum
+          asperiores. Necessitatibus, repellendus!
+        </p>
+        <a href="https://lauramayar.github.io/GDL003-Burger-Queen-Front-End/" className='btn btn-outline-success'>
+          Go inside
+        </a>
+      </div>
+    
+    </div>
+
+      
+
+
+    <div className='card text-center shadow'>
+      <div className='overflow'>
+       
+        <a href="https://surferzer.github.io/GDL003-personal-project/">
+            <img src="https://i.ibb.co/0XThBbb/competition-Sistem-2.png" width="60%" alt='DataLovers' className='card-img-top'/></a>
+      </div>
+      <div className='card-body text-dark'>
+        <h4 className='card-title'>Competition System GYM58</h4>
+        <p className='card-text text-secondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          distinctio maxime, sapiente temporibus cum quisquam totam voluptatum
+          asperiores. Necessitatibus, repellendus!
+        </p>
+        <a href="https://surferzer.github.io/GDL003-personal-project/" className='btn btn-outline-success'>
+          Go inside
+        </a>
+      </div>
+    
+    </div>
+
+
+   
+    </div>
+  
+
+ 
+
+
+
+
             {/* <div className='cypher'>
             <a href="https://surferzer.github.io/GDL003-cipher/src/" title="Cipher">
             <img src= "https://i.ibb.co/qj6dy9W/cipher.png" width="30%" alt="Cipher"/></a>
                 </div> */}
 
-                <div className='dataLovers'>
+              {/*   <div className='dataLovers'>
                <a href="https://surferzer.github.io/GDL003-data-lovers/src/" tittle="dataLovers">
                <img src="https://i.ibb.co/88t7p7t/data-Lovers-2.png" width="30%" alt="dataLovers"/> </a>
-           </div>
+           </div> */}
 
            {/* <div className='socialNetwork'>
                <a href="https://cstgarcia1204.githum.io/GDL003-social-network/src/" tittle="socialNetwork">
                <img src="https://i.imgur.com/EHbKcaC.jpg" width="30%" alt="socialNetwork"/> </a>
            </div> */}
 
-           <div className='labNotes'>
+          {/*  <div className='labNotes'>
                <a href="https://surferzer.github.io/GDL003-lab-notes/" tittle="labNotes">
                <img src="https://i.ibb.co/Jj1K1JX/labnotes-2.png" width="30%" alt="labNotes"/> </a>
-           </div>
+           </div> */}
 
-           <div className='ciboulette'>
+          {/*  <div className='ciboulette'>
                <a href="https://lauramayar.github.io/GDL003-Burger-Queen-Front-End/" tittle="ciboulette">
                <img src="https://i.ibb.co/G360XKF/ciboulette-2.png" width="30%" alt="ciboulette"/> </a>
-           </div>
+           </div> */}
 
-           <div className='competitionSystem'>
+         {/*   <div className='competitionSystem'>
                <a href="https://surferzer.github.io/GDL003-personal-project/" tittle="competitionSystem">
                <img src="https://i.ibb.co/0XThBbb/competition-Sistem-2.png" width="30%" alt="competitionSystem"/> </a>
            </div>
-           
+            */}
 
 
 
