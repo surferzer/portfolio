6ad4590e6f7e3e9987690fdeb340cc83c124bf20
surferzer <mafernandazr@gmail.com>
2019-11-22 18:24:34 -0600

    se agregan iconos de contacto sin ligas

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,25 @@ class Contact extends React.Component {
 
       
       <div class="contacting">
-        <h2>Contact</h2>
+        <p className="titleContact">Contact me!</p>
         <div className= 'contacto'>
-               <h2>Email: <a href='mafernandazr@gmail.com'>mafernandazr@gmail.com</a></h2>
-               <h2>Github: <a href='https://github.com/surferzer'>https://github.com/surferzer</a></h2>
+          <div className="email">
+               <h2> <a href='mafernandazr@gmail.com'><img src="https://images.vexels.com/media/users/3/157444/isolated/preview/cac65f0418e3896e226f803751364216-icono-de-sobre-de-marketing-by-vexels.png" width="20%" hover="mafernandazr@gmail.com"/><br></br>
+               mafernandazr@gmail.com</a></h2>
+               </div>
+
+               <div className="gitHub">
+               <h2>Github: <a href='https://github.com/surferzer'><img src="https://www.iconsdb.com/icons/preview/orange/github-6-xxl.png" width="20%" /></a></h2>
+               </div>
+
+               <div className="linkedIn">
+               <h2>Linked In: <a href='https://linkedin.com/fernandazermeno'><img src="https://iconsplace.com/wp-content/uploads/_icons/ffa500/256/png/linkedin-2-icon-11-256.png" width="20%" /></a></h2>
+               </div>
+
+               <div className="cv">
+               <h2>Cv: <a href=''><img src="https://www.u.com.my/sites/all/themes/zurb-foundation/images/umobile/selfcare_login_pic.png" width="30%" /></a></h2>
+               </div>
+
            </div>
       </div>
     );
